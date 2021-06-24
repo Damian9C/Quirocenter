936efd50f1beb37a2b791029bfbcf85ea31e2a9a
Carlos Damian Chavez Gonalez <carlos_dami12@live.com>
2021-06-24 15:27:13 -0500

    ya hay BD x2

--- src/util/index.js
+++ src/util/index.js
@@ -2,13 +2,12 @@ import firebase from "firebase/app";
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBvMvLNAqyUD5CEZs5DpUN9c1rPASt_hkw",
-    authDomain: "scholl-gestor.firebaseapp.com",
-    databaseURL: "https://scholl-gestor-default-rtdb.firebaseio.com",
-    projectId: "scholl-gestor",
-    storageBucket: "scholl-gestor.appspot.com",
-    messagingSenderId: "326131924008",
-    appId: "1:326131924008:web:63f5999e5533267125c91d"
+    apiKey: "AIzaSyBlcacO30kvgcMd6Wf2-kNgJ2r_zKTUrXo",
+    authDomain: "quirocenter-2dadb.firebaseapp.com",
+    projectId: "quirocenter-2dadb",
+    storageBucket: "quirocenter-2dadb.appspot.com",
+    messagingSenderId: "565384727979",
+    appId: "1:565384727979:web:f1484298107308144416ee"
 };
 firebase.initializeApp(firebaseConfig);
 
